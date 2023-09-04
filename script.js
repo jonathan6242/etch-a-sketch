ade2972f073f3703bf093d769d34c71aecfc14a7
Jonathan Wong <jonathanfwong@gmail.com>
2023-09-04 12:28:04 +1000

    mobile bug fixes

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let currentMode = "default";
 let currentColor = "#000";
 
 function draw(e) {
-  if (e.buttons === 1 || e.buttons === 3) {
+  alert(e.buttons);
+  if (e.buttons === 1 || e.buttons === 3 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     switch (currentMode) {
       case "grabber":
         break;
