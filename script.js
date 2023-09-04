dfde645fea329bae7e70d9823bfba2e0cd25b3e3
Jonathan Wong <jonathanfwong@gmail.com>
2023-09-04 12:31:34 +1000

    mobile bug fixes

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let currentColor = "#000";
 
 function draw(e) {
   if (e.buttons === 1 || e.buttons === 3 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    alert(currentMode, e.target.style.backgroundColor);
     switch (currentMode) {
       case "grabber":
         break;
